eb95300d49a0e27acf9054f8a3cfaf11dcfa58d9
tash-9 <3222.tasfiaislam@gmail.com>
2026-07-20 12:38:42 +0600

    CORS fixed for deployment

--- src/index.js
+++ src/index.js
@@ -12,11 +12,10 @@ import statsRoutes from "../routes/stats.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: [
-  "http://localhost:5173",
-  "https://spend-wise-client-pi.vercel.app",
-  "https://spend-wise-client-git-main-tasfia-islam-raisha-s-projects.vercel.app",],
-  credentials: true }));
+app.use(cors({
+  origin: "*",
+  credentials: false,
+  }));
 app.use(express.json({ limit: "4mb" }));
 
 // Health check
